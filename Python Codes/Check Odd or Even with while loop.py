e19bf9eda8063b2d8d3e464e99118f83c707a13c
Kushagra Keshari <64693694+KushagraK7@users.noreply.github.com>
2021-09-03 12:24:28 +0530

    Add files via upload

---
+++ Python Codes/Check Odd or Even with while loop.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 12:17:16 2021
+
+@author: Lenovo
+"""
+
+n = int(input("Please enter number "))
+
+nn = n
+
+if(n < 0): n = n*(-1)#If no is -ve, convert it to +ve before testing
+
+while(n >0):
+    n-=2
+    
+    
+if(n): print(nn, "is odd")
+
+else: print(nn, "is even")
